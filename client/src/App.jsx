45e28f91345179d791e12b22d4414b7974d68f9a
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-02-03 13:44:41 +1000

    imported and added components into the app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import Project from './components/Project'
 import Stage from './components/Stage'
 import Trade from './components/Trade'
+import Task from './components/Task'
+import ProjectManager from './components/ProjectManager'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <h1>App</h1>
       <Project />
       <Stage />
       <Trade />
-      <h1>App</h1>
+      <Task />
+      <ProjectManager />
     </div>
   )
 }
---
+++ client/src/components/ProjectManager.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectManager = () => {
+    return(
+        <div>
+            <h1>ProjectManager</h1>
+        </div>
+    )
+}
+
+export default ProjectManager
