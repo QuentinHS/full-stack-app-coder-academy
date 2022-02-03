094c344966d310cbc0706e323dc2c17819b6bf3b
QuentinHS <quentin.halysummerfield@gmail.com>
2022-02-03 22:39:27 +1000

    added logout route

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -48,7 +48,14 @@ const login = async(req, res) => {
   attachCookiesToResponse({ res, user: tokenUser })
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser })
+}
 
+const logout = async(req,res) => {
+  res.cookie("token", "logout", {httpOnly: true,
+  expires: new Date(Date.now() + 5 * 1000 )}
+  )
+  
+  res.status(StatusCodes.OK).json({msg: "user logged out"})
 
 }
 
@@ -77,4 +84,5 @@ const login = async(req, res) => {
 module.exports = {
   register,
   login,
+  logout
 }
