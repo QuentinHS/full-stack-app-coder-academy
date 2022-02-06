8fa8bd0bfb352863fd880432c820023bba703390
QuentinHS <quentin.halysummerfield@gmail.com>
2022-02-06 20:25:25 +1000

    added authorisation for fetching single user route

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User")
 const { StatusCodes } = require("http-status-codes")
 const CustomError = require("../errors")
-const { createTokenUser, attachCookiesToResponse } = require("../utils")
+const { createTokenUser, attachCookiesToResponse, checkPermissions } = require("../utils")
 
 // Get all project managers 
 
@@ -45,13 +45,14 @@ const showCurrentUser = async (req, res) => {
 }
 
 const getSingleUser = async (req, res) => {
-
   const user = await User.findOne({ _id: req.params.id }).select("-password")
   if (!user) {
     throw new CustomError.NotFoundError(`No user with id: ${req.params.id} `)
   }
-  res.status(StatusCodes.OK).json({user})
+  checkPermissions(req.user, user._id)
+  res.status(StatusCodes.OK).json({ user })
 }
+
 
 // update user
 const updateUser = async (req, res) => {
--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -1,10 +1,12 @@
 const createTokenUser = require("./createTokenUser")
 const { createJWT, isTokenValid, attachCookiesToResponse } = require("./jwt")
+const checkPermissions = require("./checkPermissions")
 
 module.exports = {
   createJWT,
   isTokenValid,
   attachCookiesToResponse,
-  createTokenUser
+  createTokenUser,
+  checkPermissions
 }
 
