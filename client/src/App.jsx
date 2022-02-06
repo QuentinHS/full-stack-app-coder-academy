01ddf69c005f1b9685552b6e8196a32b4958d7b1
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-02-06 15:05:36 +1000

    corrected discrepencies after pulling from repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,42 +10,14 @@ import User from './components/User'
 import Nav from './components/Nav'
 import Home from './components/Home'
 import Footer from './components/Footer'
-<<<<<<< HEAD
-import Landing from "./components/Landing"
-import ScrollToTop from "./components/ScrollToTop"
-=======
 import Register from './components/Register'
 import Login from './components/Login'
->>>>>>> 0f1f9c46ace8728a630ef9fe024ff0c2eddbd469
 
 function App() {
   const [count, setCount] = useState(0)
  
 
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <BrowserRouter >
-        <Nav />
-        <h1>App</h1>
-
-{/* Routes are not yet defined properly */}
-
-        <Routes >
-          <Route path="/" element={<Landing />}/>
-          <Route path="/project" element={<Project />}/>
-          <Route path="/stage/:id" element={<Stage />}/>
-          <Route path="/trade/:id" element={<Trade />}/>
-          <Route path="/task/:id" element={<Task />}/>
-          {/* Need to find user by :id  */}
-          <Route path="/user" element={<User />}/>
-        </Routes>
-        <ScrollToTop />
-        {/* commented out because undesided on how I want to make it stay on bottom of page */}
-        {/* <Footer /> */}
-      </BrowserRouter> 
-    </div>
-=======
     < BrowserRouter>
       <Nav />
       <Routes>
@@ -62,7 +34,6 @@ function App() {
       <Footer />
     </BrowserRouter>
    
->>>>>>> 0f1f9c46ace8728a630ef9fe024ff0c2eddbd469
   )
 }
 
