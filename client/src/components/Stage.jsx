e68229e7f2f7c73bedb9c103a39c9d78ddf0157b
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-02-07 20:16:25 +1000

    created heading for stage

--- client/src/components/Stage.jsx
+++ client/src/components/Stage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Input, InputLeftAddon, InputGroup, Center, Button, Text } from '@chakra-ui/react'
 
 const Stage = () => {
+
+    const projectList = {projects: [{address: "123 fake street"}, {address: "555 Evergreen trc"}, {address: "31 Spooner st"}]}
+    const pm = {address: "123 fake street", task: [{stageNum: 1, taskNum: 1, trade: "carentry", provider: "Johno's construction"},{stageNum: 2, taskNum: 2, trade: "2 carentry", provider: "2 Johno's construction"}, {stageNum: 3, taskNum: 3, trade: "3 carentry", provider: "7 Johno's construction"}]}
+
+    const currentProject = pm
+
     return(
         <div>
-            <h1>Stage</h1>
+            <Center>
+                <Text mb='1rem' fontSize='3xl' color='teal' as='b'>{currentProject.address}</Text>
+            </Center>
         </div>
     )
 }
