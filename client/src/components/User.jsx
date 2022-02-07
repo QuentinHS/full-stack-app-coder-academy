e1a85900aa0f8eda1eb5bb800bfd9a6eb0754903
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-02-07 10:27:36 +1000

    Added Text and buttons to User component

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {InputRightElement, InputGroup, Center, Input, Text,FormControl, FormLabel, FormErrorMessage, FormHelperText, Button, VStack, HStack} from '@chakra-ui/react'
 import { useFormik } from "formik"
 import validate from "../validation/signupValidation"
-
+import{ Link } from 'react-router-dom'
 
 
 
@@ -29,8 +29,46 @@ const User = () => {
     })  
     return(
         <div>
-            <h1>User</h1>
+            <Center>
+                {/* User name and other data will need to retrived and parsed in*/}
+                <Text mb='1rem' fontSize='5xl' color='teal' as='b'>User</Text>
+            </Center>
+            {/* Maybe change position to something that isn't centered */}
+            <Center>
+                <Text mb='1rem' fontSize='2xl' color='teal' as='b'>Email:</Text>
+            </Center>
+            <Center>
+                <Text mb='1rem' fontSize='2xl' color='teal' as='b'>Position:</Text>
+            </Center>
+            <Center>
+                <Text mb='1rem' fontSize='2xl' color='teal' as='b'>Trade:</Text>
+            </Center>
+            <Center>
+                <Text mb='1rem' fontSize='2xl' color='teal' as='b'>Company:</Text>
+            </Center>
+            <Center>
+                <Text mb='1rem' fontSize='2xl' color='teal' as='b'>ABN:</Text>
+            </Center>
+            <Center>
+                <Link to="/register">
+                    <Button mt="1em" width="15em" colorScheme="teal" variant="solid">Edit Profile</Button>
+                </Link>
+            </Center>
+            <Center>
+                <Link to="/register">
+                    <Button mt="1em" width="15em" colorScheme="teal" variant="solid">Change Password</Button>
+                </Link>
+            </Center>
+            <Center>
+                <Link to="/register">
+                    <Button mt="1em" width="15em" colorScheme="teal" variant="solid">Delete Account</Button>
+                </Link>
+            </Center>
         </div>
+
+
+
+
     )
 
 }
