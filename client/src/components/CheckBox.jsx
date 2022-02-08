a336561357fc055123d7d06e24886094e7a8b3d2
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-02-08 11:58:12 +1000

    made the checkbox toggle true and false

--- client/src/components/CheckBox.jsx
+++ client/src/components/CheckBox.jsx
@@ -1,87 +1,22 @@
-import React from "react";
-import { useState } from "react"
-import "./CheckBox.css"
+import { useState } from 'react';
 
-const CheckBox = () => {
+function CheckBox () {
 
-// const stages = [{stage1: false}, {stage2: false}, {stage3: false}, {stage4: false}]
-const stages = [{stage: "stage1"}, {stage: "stage2"}, {stage: "stage3"}, {stage: "stage4"}]
+  const [checked, setChecked] = useState(false);
 
-  const [checkedState, setCheckedState] = useState(
-    new Array(stages.length).fill(false)
-  )
-
-  const handleOnChange = (position) => {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item 
-  )
-  setCheckedState(updatedCheckedState)
+  function toggle(value){
+    return !value;
   }
 
-  
-
-    return (
-        <div className="App">
-            <h3>Select Toppings</h3>
-            <ul className="toppings-list">
-            {stages.map(( stage , index) => {
-                return (
-                <li key={index}>
-                    <div className="toppings-list-item">
-                    <div className="left-section">
-                        <input
-                        type="checkbox"
-                        id={`custom-checkbox-${index}`}
-                        name={stage}
-                        value={stage}
-                        checked={checkedState[index]}
-                        onChange={() => handleOnChange(index)}
-                        />
-                        <label htmlFor={`custom-checkbox-${index}`}>{stage}</label>
-                    </div>
-                    {/* <div className="right-section">{getFormattedPrice(price)}</div> */}
-                    </div>
-                </li>
-                );
-            })}
-            
-            <li>
-                <div className="toppings-list-item">
-                <div className="left-section">Total:</div>
-                {/* <div className="right-section">{getFormattedPrice(total)}</div> */}
-                </div>
-            </li>
-            </ul>
-        </div>
-    )
+  return (
+    <form>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={() => setChecked(toggle)}
+      />
+    </form>
+  )
 }
 
 export default CheckBox
-
-
-
-
-
-
-
-
-// export default function App() {
-
-
-//   const [total, setTotal] = useState(0);
-
-
-
-
-//     const totalPrice = updatedCheckedState.reduce(
-//       (sum, currentState, index) => {
-//         if (currentState === true) {
-//           return sum + toppings[index].price;
-//         }
-//         return sum;
-//       },
-//       0
-//     );
-
-//     setTotal(totalPrice);
-//   };
