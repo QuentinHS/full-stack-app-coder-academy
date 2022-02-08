be438a5d9c5839f91c2121cfc32271a55b267fac
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-02-08 16:03:01 +1000

    Created data for the trade provider component

--- client/src/components/TradeProviders.jsx
+++ client/src/components/TradeProviders.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Input, InputLeftAddon, InputGroup, Center, Button, Text, Icon, Checkbox, Stack } from '@chakra-ui/react'
+import { AiOutlinePlusCircle, AiFillExclamationCircle } from 'react-icons/ai'
+import { BsTrash, BsPlusCircle } from 'react-icons/bs'
+import{ Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+
+const tradeList = [{carpenter: ["Jim’s carpentry", "JVK & Sons Construction", "Josh Cassidy Carpenter"]}, {plumber: ["Jim’s plumbing", "Paul Fitzpatrick Plumbing", "Damien's Plumbing Services"]}, {electrical: ["Jim’s electrical", "On Electrical Contractors Pty Ltd", "NNC ELECTRICAL"]}]
+
+
 
 const TradeProdvider = () => {
 
     return(
-        null
+        <div>
+            <Center>
+                <Icon m="1rem" as={BsPlusCircle} />
+                <Link to="/">  Add trade provider </Link>
+            </Center>
+        </div>
     )
 } 
 
