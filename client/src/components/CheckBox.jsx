66656fe2b807730b0edc478e7df5f03df56bd2e3
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-02-08 11:16:01 +1000

    fixed import export error in checkbox component

--- client/src/components/CheckBox.jsx
+++ client/src/components/CheckBox.jsx
@@ -1,23 +1,30 @@
 import React from "react";
+import { useState } from "react"
+import "./CheckBox.css"
 
 const CheckBox = () => {
 
-//   const [checkedState, setCheckedState] = useState(
-//     new Array(stages.length).fill(false)
-//   );
+// const stages = [{stage1: false}, {stage2: false}, {stage3: false}, {stage4: false}]
+const stages = [{stage: "stage1"}, {stage: "stage2"}, {stage: "stage3"}, {stage: "stage4"}]
 
-//   const handleOnChange = (position) => {
-//     const updatedCheckedState = checkedState.map((item, index) =>
-//       index === position ? !item : item
-//     );
+  const [checkedState, setCheckedState] = useState(
+    new Array(stages.length).fill(false)
+  )
 
-//     setCheckedState(updatedCheckedState);
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item 
+  )
+  setCheckedState(updatedCheckedState)
+  }
+
+  
 
     return (
-        <div className="checkBox">
+        <div className="App">
             <h3>Select Toppings</h3>
             <ul className="toppings-list">
-            {toppings.map(({ name, price }, index) => {
+            {stages.map(( stage , index) => {
                 return (
                 <li key={index}>
                     <div className="toppings-list-item">
@@ -25,22 +32,23 @@ const CheckBox = () => {
                         <input
                         type="checkbox"
                         id={`custom-checkbox-${index}`}
-                        name={name}
-                        value={name}
+                        name={stage}
+                        value={stage}
                         checked={checkedState[index]}
                         onChange={() => handleOnChange(index)}
                         />
-                        <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
+                        <label htmlFor={`custom-checkbox-${index}`}>{stage}</label>
                     </div>
-                    <div className="right-section">{getFormattedPrice(price)}</div>
+                    {/* <div className="right-section">{getFormattedPrice(price)}</div> */}
                     </div>
                 </li>
                 );
             })}
+            
             <li>
                 <div className="toppings-list-item">
                 <div className="left-section">Total:</div>
-                <div className="right-section">{getFormattedPrice(total)}</div>
+                {/* <div className="right-section">{getFormattedPrice(total)}</div> */}
                 </div>
             </li>
             </ul>
