7f85f1806e508ed1e5718d0039d59fe7d4d0cdb0
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-02-09 21:46:22 +1000

    Added TextArea to NewTask component

--- client/src/components/NewTask.jsx
+++ client/src/components/NewTask.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Center, Icon, Text, Input,  Heading, IconButton, Alert, AlertDescription, AlertIcon, FormControl, FormLabel, FormErrorMessage, FormHelperText, Button } from '@chakra-ui/react'
+import { Center, Icon, Text, Textarea, Input,  Heading, IconButton, Alert, AlertDescription, AlertIcon, FormControl, FormLabel, FormErrorMessage, FormHelperText, Button } from '@chakra-ui/react'
 import { useFormik } from "formik"
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import{ Link } from 'react-router-dom'
@@ -25,7 +25,7 @@ const NewTask = () => {
                 <Heading fontSize='5xl' color='teal' as='b' >Create New Task</Heading>
             </Center>
             <Center >
-                <FormControl isInvalid={formik.errors.trade}>
+                <FormControl width='80%' isInvalid={formik.errors.trade}>
                     <FormLabel mt='2rem' htmlFor='businessName'>Task Name </FormLabel>
                     <Input id='businessName' name='businessName' placeholder='Enter a task Name' onChange={formik.handleChange} value={formik.values.businessName}/>
                     {formik.errors.businessName ? <FormErrorMessage>{formik.errors.businessName}</FormErrorMessage> : null}
@@ -39,6 +39,11 @@ const NewTask = () => {
                 <Text>Task Assignee</Text>
                 <IconButton size='sm'  mr="2rem" ml='12px' aria-label='Show/hide component' icon={<ChevronDownIcon/>} onClick={handleClickShow}/>
             </Center>
+
+            <Center>
+                <Textarea width='80%'  placeholder='Here is a sample placeholder' />
+            </Center>
+
             <Center mt='2rem'>
                 <Link to="/register">
                     <Button mt="1em" width="15rem" colorScheme="teal" variant="solid">Add Task</Button>
