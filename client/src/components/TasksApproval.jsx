fe563e843fbad46a26ac18fd3444fbfdae3bd79a
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-02-10 12:00:10 +1000

    Created list of task data and implemented it as text displayed in TaskApproval component

--- client/src/components/TasksApproval.jsx
+++ client/src/components/TasksApproval.jsx
@@ -1,8 +1,14 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Center, IconButton, List, ListItem, Text, VStack } from "@chakra-ui/react";
+import { Center, IconButton, List, ListItem, Text, VStack, Heading, Button } from "@chakra-ui/react";
 import React from "react";
+import{ Link } from 'react-router-dom'
 
 const TasksApproval = ({tasksList=[]}) =>{
+
+
+    const task = {address: "123 Fake street", stage: [{stageNum: 1, taskName: "foo", trade: "carentry", provider: "Johno's construction", complete: false},{stageNum: 2, taskName: "bar", trade: "2 carentry", provider: "2 Johno's construction"}, {stageNum: 3, taskName: "blah", trade: "3 carentry", provider: "7 Johno's construction"}, {stageNum: 5, taskName: "blah", trade: "3 carentry", provider: "7 Johno's construction"}]}
+
+    const currentTask = task
 
     const [show, setShow] = React.useState(false)
     const handleClickShow = () => setShow(!show)
@@ -10,30 +16,22 @@ const TasksApproval = ({tasksList=[]}) =>{
    
 
     return (
-        <>
-            <VStack>
-
+        <div>
             <Center>
-
-                <Text> Tasks to Approve </Text>
-                <IconButton size='sm' ml='8px' aria-label='Show/hide component' icon={<ChevronDownIcon />} onClick={handleClickShow}/>
+                <Heading mb='1rem' fontSize='5xl' color='teal' as='b'>Task Approval</Heading>
+            </Center>
+            <Center >
+                <Text mb='1rem' fontSize='md' as='b'>Stage: {currentTask.stage[0].stageNum}</Text>
+            </Center>
+            <Center >
+                <Text mb='1rem' fontSize='3xl' as='b'>Task: {currentTask.stage[0].taskName}</Text>
+            </Center>
+            <Center >
+                <Text mb='1rem' fontSize='2xl' as='b'>Submitted by: {currentTask.stage[0].provider}</Text>
             </Center>
 
-            <List spacing={3}>
-                {tasksList.map((data, index)=>{
-                    if (data){
-                        return  show ? (
-                            <Center>
-                                <ListItem> {data.project.name} {data.stage} { data.name} </ListItem>
-                            </Center>
-                        ): null
-                    }
-                    return null
-                })}
-            </List>
-        
-            </VStack>
-        </>
+
+        </div>
     )
 }
 
